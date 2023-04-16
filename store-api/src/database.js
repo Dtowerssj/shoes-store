6ba34db115a1d1078c8792f4e52ec25db83dce96
Diego Torres <diego31199@gmail.com>
2023-04-16 03:36:52 -0400

    render changes

--- store-api/src/database.js
+++ store-api/src/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://dtorres:ieWLUH6fPWdbzNg4@cluster0.m7nq5.mongodb.net/store-db')
+mongoose.connect('mongodb+srv://dtowers:17camarones@cluster0.m7nq5.mongodb.net/store-db')
     .then(db => console.log('DB is connected'))
     .catch(error => console.log(error))
